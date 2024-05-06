7954bd2d4004e2db02f0404ad0170508c1a38f68
Habeeb Mac-iver <hmaciver@agilenceince.com>
2024-05-06 14:53:17 +0100

    Add types in a different file

--- src/lib/xstream-codes/player.ts
+++ src/lib/xstream-codes/player.ts
@@ -1,41 +1,9 @@
-export type PlayerApiConfig = {
-  baseUrl: string;
-  auth: { username: string; password: string };
-};
-
-export type Filter = {
-  category_id?: string;
-  vod_id?: string | number;
-  stream_id?: any;
-  limit?: any;
-};
-
-export interface Category {
-  category_id: number;
-  category_name: string;
-  parent_id: number;
-  streams: Stream[];
-}
-
-export type Stream = {
-  added: Date;
-  category_id: number;
-  category_name: string;
-  container_extension: string;
-  custom_sid: string;
-  direct_source: string;
-  epg_channel_id: string;
-  stream_icon: string;
-  stream_id: number;
-  url: string;
-  name: string;
-  num: number;
-  rating: number;
-  rating_5based: number;
-  tv_archive: number;
-  tv_archive_duration: number;
-  stream_type: string;
-};
+import { 
+  type Stream,
+  type Category,
+  type PlayerApiConfig,
+  type Filter,
+} from "./types";
 
 /**
  * @version 2.x
