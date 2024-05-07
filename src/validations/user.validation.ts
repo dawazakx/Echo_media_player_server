6bdead23c49c3e430a1c3b91d530fcd8dd29af09
kelechi <igwesiemmanuel4@gmail.com>
2024-05-07 23:52:42 +0100

    updated validation file

--- src/validations/user.validation.ts
+++ src/validations/user.validation.ts
@@ -4,11 +4,9 @@ const connectX = Joi.object({
   device: Joi.object({
     id: Joi.string().required().messages({
       "any.required": "Device ID is required",
-      "string.empty": "Device ID must not be empty",
     }),
     type: Joi.string().required().messages({
       "any.required": "Device type is required",
-      "string.empty": "Device type must not be empty",
     }),
   })
     .required()
@@ -17,19 +15,15 @@ const connectX = Joi.object({
     }),
   nickname: Joi.string().required().messages({
     "any.required": "Nickname is required",
-    "string.empty": "Nickname must not be empty",
   }),
   username: Joi.string().required().messages({
     "any.required": "Username is required",
-    "string.empty": "Username must not be empty",
   }),
   password: Joi.string().required().messages({
     "any.required": "Password is required",
-    "string.empty": "Password must not be empty",
   }),
   url: Joi.string().required().messages({
     "any.required": "URL is required",
-    "string.empty": "URL must not be empty",
   }),
 });
 
