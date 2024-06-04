f33887917dc1991e7e97411f4f0773524eaf5c4e
kelechi <igwesiemmanuel4@gmail.com>
2024-06-04 15:37:12 +0100

    updated and remove access token on the register endpoint

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,12 +1,10 @@
 import { Response } from "express";
 import IUser from "../interfaces/user.interface";
 import UserModel from "../models/user.model";
-import { generateToken } from "../utils/generateToken";
 import { sendOtp } from "./sendOtp.service";
 
 const createUser = async (userData: IUser, res: Response) => {
   try {
-    // Check if a user with the same email already exists
     const existingEmailUser = await UserModel.findOne({
       email: userData.email,
     });
@@ -18,27 +16,20 @@ const createUser = async (userData: IUser, res: Response) => {
       };
     }
 
-    // Create an instance of UserModel
-    const newUserInstance = new UserModel({
+    const user = new UserModel({
       ...userData,
       isVerified: false,
     });
 
-    // Generate a token for the user
-    const token = generateToken(newUserInstance);
+    const { otp } = await sendOtp(user.email, user.firstName);
 
-    // Send OTP and get expiration time
-    const { otp } = await sendOtp(newUserInstance.email, newUserInstance.firstName);
-
-    // Save the user with the token and false isVerified
-    newUserInstance.token = token;
-    await newUserInstance.save();
+    await user.save();
 
     // Set OTP in server cookie to expire in one hour
     res.cookie("otp", otp, { httpOnly: true, maxAge: 3600000 });
 
     // Omit the password field from the returned user data
-    const { password, ...userDataWithoutPassword } = newUserInstance.toObject();
+    const { password, ...userDataWithoutPassword } = user.toObject();
 
     return {
       message: "OTP sent to your email",
